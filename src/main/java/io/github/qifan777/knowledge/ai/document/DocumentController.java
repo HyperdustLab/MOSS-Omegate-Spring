be311023302b70cfe61c351828cc709325efdc6b
fangyeming <fangyemimgzz@gmail.com>
2025-02-07 10:00:35 +0800

    1

--- src/main/java/io/github/qifan777/knowledge/ai/document/DocumentController.java
+++ src/main/java/io/github/qifan777/knowledge/ai/document/DocumentController.java
@@ -1,5 +1,6 @@
 package io.github.qifan777.knowledge.ai.document;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.ai.document.Document;
@@ -13,31 +14,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @RequestMapping("document")
 @RestController
 @AllArgsConstructor
 public class DocumentController {
-    private final VectorStore vectorStore;
 
-    /**
-     * 嵌入文件
-     *
-     * @param file 待嵌入的文件
-     * @return 是否成功
-     */
-    @SneakyThrows
-    @PostMapping("embedding")
-    public Boolean embedding(@RequestParam MultipartFile file) {
-        // 从IO流中读取文件
-        TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(new InputStreamResource(file.getInputStream()));
-        // 将文本内容划分成更小的块
-        List<Document> splitDocuments = new TokenTextSplitter()
-            .apply(tikaDocumentReader.read());
-        // 存入向量数据库，这个过程会自动调用embeddingModel,将文本变成向量再存入。
-        vectorStore.add(splitDocuments);
-        return true;
-    }
+
+  private final VectorStore vectorStore;
+
+  /**
+   * 嵌入文件
+   *
+   * @param file 待嵌入的文件
+   * @return 是否成功
+   */
+  @SneakyThrows
+  @PostMapping("embedding")
+  public Boolean embedding(@RequestParam MultipartFile file) {
+    // 从IO流中读取文件
+    TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(
+        new InputStreamResource(file.getInputStream()));
+    // 将文本内容划分成更小的块
+    List<Document> splitDocuments = new TokenTextSplitter()
+        .apply(tikaDocumentReader.read());
+    // 存入向量数据库，这个过程会自动调用embeddingModel,将文本变成向量再存入。
+    vectorStore.add(splitDocuments);
+    return true;
+  }
 
 }
